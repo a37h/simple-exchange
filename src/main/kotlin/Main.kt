6673c4f822c754c946fddb1a303169685990be8a
a37h <justfastmeow@gmail.com>
2023-10-16 11:16:40 +0200

    Add new orders into simple order books without matching them

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import java.util.TreeSet
+
 data class Order(val orderId: String, val side: Char, val price: Int, val quantity: Int)
 
 fun parseOrder(s: String): Order {
@@ -11,12 +13,34 @@ fun parseOrder(s: String): Order {
     return Order(orderId, side, price, quantity)
 }
 
+data class Limit(val arrivalIndex: Int, val orderId: String, val price: Int, val quantity: Int)
+
+fun processOrder(bids: TreeSet<Limit>, asks: TreeSet<Limit>, arrivalIndex: Int, o: Order) {
+    println("#: $arrivalIndex order: $o")
+    if (o.side == 'B') {
+        bids.add(Limit(arrivalIndex, o.orderId, o.price, o.quantity))
+    }
+    if (o.side == 'S') {
+        asks.add(Limit(arrivalIndex, o.orderId, o.price, o.quantity))
+    }
+}
+
 fun main() {
+    // arrivalIndex - represents order arrival chronology for matching orders in price time priority
+    var arrivalIndex = 0
+
+    val bids = TreeSet(compareByDescending<Limit> { it.price }.thenBy { it.arrivalIndex })
+    val asks = TreeSet(compareBy<Limit> { it.price }.thenBy { it.arrivalIndex })
+
     var inputString = readlnOrNull()
 
     while (inputString != null) {
-        var order = parseOrder(inputString)
-        println(order)
+        val order = parseOrder(inputString)
+        arrivalIndex += 1
+        processOrder(bids, asks, arrivalIndex, order)
         inputString = readlnOrNull()
     }
+
+    println("Bids: $bids")
+    println("Asks: $asks")
 }
