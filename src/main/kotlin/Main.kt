1c4092e38bb8c73302e968e4dd314ce56db17308
a37h <justfastmeow@gmail.com>
2023-10-16 07:54:19 +0200

    Parse orders from the input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,22 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+data class Order(val orderId: String, val side: Char, val price: Int, val quantity: Int)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun parseOrder(s: String): Order {
+    val order = s.split(",")
+
+    val orderId:String=order[0]
+    val side:Char=order[1].first()
+    val price:Int=order[2].toInt()
+    val quantity:Int=order[3].toInt()
+
+    return Order(orderId, side, price, quantity)
+}
+
+fun main() {
+    var inputString = readlnOrNull()
+
+    while (inputString != null) {
+        var order = parseOrder(inputString)
+        println(order)
+        inputString = readlnOrNull()
+    }
 }
